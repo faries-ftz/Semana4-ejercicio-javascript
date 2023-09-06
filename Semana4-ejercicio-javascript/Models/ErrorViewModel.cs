cacd32faa583a9658b6a247cef75b9c06aeb11be
Alumno <Alumno@DESKTOP-L1V7HS3>
2023-09-05 22:33:54 -0500

    Agregar archivos de proyecto.

---
+++ Semana4-ejercicio-javascript/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Semana4_ejercicio_javascript.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
